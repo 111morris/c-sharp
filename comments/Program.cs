1b76b3f53a5ffdaa5fea91f2db378c1ef5bf204d
Morris Mulandi <mulandimorris1@gmail.com>
2024-10-15 12:41:07 +0300

    comments

---
+++ comments/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+Random random = new Random();
+
+string[] orderIDs = new string[5];
+
+for (int i = 0; i < orderIDs.Length; i++)
+{
+ // get a random number that equates to ASCII letters A through E
+ int prefixValue = random.Next(65, 81);
+ string prefix = Convert.ToChar(prefixValue).ToString();
+ string suffix = random.Next(1, 1000).ToString("000");
+ orderIDs[i] = prefix + suffix;
+}
+
+foreach (var orderID in orderIDs)
+{
+ Console.WriteLine(orderID);
+}
