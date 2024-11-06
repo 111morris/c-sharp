ebfa330c6d93f9766d0f86107524f13aeac97687
111morris <mulandimorris1@gmail.com>
2024-11-06 14:55:57 +0300

    do while project 2

---
+++ do-while-project2/Program.cs
@@ -0,0 +1,18 @@
+﻿// a code that validates a string input 
+
+Console.WriteLine("Enter your role name (Administrator, Manager, or user)");
+
+do
+{
+ string? input = Console.ReadLine();
+
+ if (input.Trim().ToLower() == "admnistrator" || input.Trim().ToLower() == "manager" || input.Trim().ToLower() == "user")
+ {
+  Console.WriteLine($"Your input value ({input}) has been accepted");
+  break;
+ }
+ else
+ {
+  Console.WriteLine($"The role that you entered, \"{input.ToLower()}\" is not valid. Enter your role name (Administrator, Manager, User)");
+ }
+} while (true);
