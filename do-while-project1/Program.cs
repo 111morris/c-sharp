9c67ed96718686eb7c97dec4d944211a680a568b
111morris <mulandimorris1@gmail.com>
2024-11-06 13:39:32 +0300

    do while project 1

---
+++ do-while-project1/Program.cs
@@ -0,0 +1,26 @@
+﻿// A code that validates integer input 
+using System.Linq.Expressions;
+
+Console.WriteLine("Please enter an integer value between 5 and 10");
+
+int number;
+bool isValid;
+
+do
+{
+ string? input = Console.ReadLine();
+ isValid = int.TryParse(input, out number);
+ if (isValid && number >= 5 && number <= 10)
+ {
+  Console.WriteLine($"Succss, you entered {number}. Your input has been accepted.");
+  break;
+ }
+ else if (!isValid)
+ {
+  Console.WriteLine("Invalid input. Please enter a valid number between 5 and 10");
+ }
+ else
+ {
+  Console.WriteLine($"You entered {number}. Please enter a nuber between 5 and 10");
+ }
+} while (true);
