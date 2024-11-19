895a80bcea0d29e249f9d45946453b66062fe875
111morris <mulandimorris1@gmail.com>
2024-11-19 20:48:13 +0300

    message 2

---
+++ challenge-project1/Program.cs
@@ -0,0 +1,11 @@
+﻿int numericValue = 0, i = 10;
+bool validNumber = false;
+string? readResult;
+
+Console.WriteLine("Please enter a value between 5 and 10");
+while (i <= 0)
+{
+ readResult = Console.ReadLine();
+ validNumber = int.TryParse(readResult, out numericValue);
+ i--;
+}
