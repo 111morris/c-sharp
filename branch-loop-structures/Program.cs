31db8896577cf88c753bed5159f1d87a2dad3d5e
111morris <mulandimorris1@gmail.com>
2024-11-19 21:50:31 +0300

    end of trophy 3

---
+++ branch-loop-structures/Program.cs
@@ -0,0 +1,47 @@
+﻿// Specification
+/* -In this challenge exercise, you need to develop a feature that ensures animal ages and physical descriptions are complete.
+
+-This feature must:
+
+-Be enabled inside the appropriate application branch (must not overwrite the code in the code branch for menu option 2).
+
+-Skip over any animal in the ourAnimals array when the value of pet ID is set to the default value.
+
+-Display the pet ID value and prompt the user for an updated data value if ourAnimals array data is missing or incomplete.
+
+-Ensure that a valid numeric value is assigned to animalAge for all animals in the ourAnimals array that have assigned data.
+
+-Ensure that a valid string is assigned to animalPhysicalDescription for all animals in the ourAnimals array that have assigned data.
+
+-Enforce the following validation rules for animalAge.
+
+-It must be possible to convert the value entered to numeric data type.
+-Enforce the following validation rules for animalPhysicalDescription:
+
+--Values cannot be null.
+--Values cannot have zero characters.
+--Any further restriction is up to the developer.
+Inform the application user when all data requirements are met, pausing the application to ensure the message can be seen and responded to. */
+
+// Specification
+/* 
+You need to develop a feature that ensures animal nicknames and personality descriptions are complete.
+
+This feature must:
+
+Be enabled inside the appropriate application branch (must not overwrite the code in the code branch for menu option 2).
+
+Skip over any animal in the ourAnimals array when the value of pet ID is set to the value default value.
+
+Display the pet ID value and prompt the user for an updated data value if ourAnimals array data is missing or incomplete.
+
+Ensure that a valid string is assigned to animalNickname for all animals in the ourAnimals array that have assigned data.
+
+Ensure that a valid string is assigned to animalPersonalityDescription for all animals in the ourAnimals array that have assigned data.
+
+Enforce the following validation rules for petNickname and petPersonalityDescription:
+
+Values cannot be null.
+Values cannot have zero characters.
+Any further restriction is up to the developer.
+Inform the application user when all data requirements are met, pausing the application to ensure the message can be seen and responded to. */
